cd1598ed587e0bffbcc85e34b61adf47db1a4f61
84384 <thanhhlongg2412@gmail.com>
2022-09-21 14:07:40 +0700

    update

---
+++ Module2/src/ss12_java_collection_framework/exersice/service/IProductService.java
@@ -0,0 +1,10 @@
+package ss12_java_collection_framework.exersice.service;
+
+public interface IProductService {
+    void addProduct();
+    void editProduct();
+    void removeProduct();
+    void displayProduct();
+    void findProduct();
+    void sortProduct();
+}
