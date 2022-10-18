2c3db967a891c01cde1214f6b874935be2569abc
84384 <thanhhlongg2412@gmail.com>
2022-10-18 11:04:59 +0700

    update

--- Module3/ss2/exersice2.sql
+++ Module3/ss2/exersice2.sql
@@ -21,4 +21,38 @@ primary key(o_id, p_id),
 foreign key (o_id) references orders(o_id),
 foreign key (p_id) references product(p_id),
 od_qty varchar(45));
-
+insert into customer 
+value (1, 'Minh Quan', 10),
+(2, 'Ngoc Oanh',20),
+(3, 'Hong Ha',50);
+insert into orders(o_id,c_id,o_date)
+value (1,1,'2006-03-21'),
+(2,2,'2006-03-23'),
+(3,1,'2006-03-16');
+insert into product
+value (1, 'May Giat',3),
+(2,'Tu Lanh',5),
+(3,'Dieu Hoa',7),
+(4,'Quat',1),
+(5,'Bep Dien',2);
+insert into order_detail 
+value (1,1,3),
+(1,3,7),
+(1,4,2),
+(2,1,1),
+(3,1,8),
+(2,5,4),
+(2,3,3);
+-- Hiển thị các thông tin  gồm oID, oDate, oPrice của tất cả các hóa đơn trong bảng Order
+select o_id,o_date,o_total_price 
+from orders;
+-- Hiển thị danh sách các khách hàng đã mua hàng, và danh sách sản phẩm được mua bởi các khách
+select customer.c_id, customer.c_name,orders.o_id,order_detail.p_id,product.p_name
+from customer join orders on customer.c_id = orders.c_id join order_detail on orders.o_id = order_detail.o_id join product on order_detail.p_id = product.p_id ;
+-- Hiển thị tên những khách hàng không mua bất kỳ một sản phẩm nào
+select customer.c_id, customer.c_name, orders.o_id
+from customer left join orders on customer.c_id = orders.c_id
+where orders.c_id is null;
+-- Hiển thị mã hóa đơn, ngày bán và giá tiền của từng hóa đơn (giá một hóa đơn được tính bằng tổng giá bán của từng loại mặt hàng xuất hiện trong hóa đơn. Giá bán của từng loại được tính = odQTY*pPrice)
+select orders.o_id, orders.o_date, order_detail.od_qty * product.p_price as tatol
+from orders join order_detail on orders.o_id = order_detail.o_id join product on order_detail.p_id = product.p_id;
--- Module3/ss2/practice3.sql
+++ Module3/ss2/practice3.sql
@@ -1,25 +1,25 @@
 create database quan_ly_sinh_vien;
 use quan_ly_sinh_vien;
 create table class (
-id_class int not null primary key auto_increment,
+id_class int primary key auto_increment,
 name_class varchar(60) not null,
 start_date datetime not null,
 status bit );
 create table student (
-student_id int not null primary key auto_increment,
+student_id int  primary key auto_increment,
 student_name varchar(30) not null,
 address varchar(50),
 phone varchar (20),
 status bit,
-id_class int not null,
+id_class int,
 foreign key (id_class) references class(id_class));
 create table subject (
-sub_id int not null primary key auto_increment,
+sub_id int  primary key auto_increment,
 sub_name varchar(30) not null,
 credit tinyint default 1 check (credit >= 1),
 status bit default 1);
 create table mark(
-mark_id int not null primary key auto_increment,
+mark_id int primary key auto_increment,
 sub_id int not null,
 student_id int not null,
 mark float default 0 check(mark between 0 and 100),
@@ -27,4 +27,48 @@ exam_time tinyint default 1,
 unique(sub_id, student_id),
 foreign key (sub_id) references subject(sub_id),
 foreign key (student_id) references student(student_id));
-
+INSERT INTO class
+VALUES (1, 'A1', '2008-12-20', 1),
+ (2, 'A2', '2008-12-22', 1),
+ (3, 'B3', current_date, 0);
+ INSERT INTO student (student_name, address, phone, status, id_class)
+VALUES ('Hung', 'Ha Noi', '0912113113', 1, 1);
+INSERT INTO student (student_name, address, status, id_class)
+VALUES ('Hoa', 'Hai phong', 1, 1);
+INSERT INTO student (student_name, address, phone, status, id_class)
+VALUES ('Manh', 'HCM', '0123123123', 0, 2);
+INSERT INTO subject
+VALUES (1, 'CF', 5, 1),
+       (2, 'C', 6, 1),
+       (3, 'HDJ', 5, 1),
+       (4, 'RDBMS', 10, 1);
+INSERT INTO mark (sub_id,student_id,mark,exam_time)
+VALUES (1, 1, 8, 1),
+       (1, 2, 10, 2),
+       (2, 1, 12, 1);
+SELECT *FROM student;	
+SELECT *
+FROM student
+WHERE status = true;
+SELECT *
+FROM subject
+WHERE credit < 10;
+select student.student_id, student.student_name, class.name_class
+from student join class on 	student.id_class = class.id_class
+where class.name_class = 'A1';
+select student.student_id, student.student_name, subject.sub_name, mark.mark
+from student join mark on student.student_id = mark.student_id join subject on subject.sub_id = mark.sub_id
+where subject.sub_name = 'CF';
+select * from student 
+where student_name like 'H%';
+select * from class
+where month(start_date) = 12;
+select * from subject
+where credit between 3 and 5;
+SET SQL_SAFE_UPDATES = 0; -- tắt chế độ an toàn
+update student 
+set id_class = 2
+where student_name = 'Hung';
+select student.student_name , subject.sub_name, mark.mark 
+from student join mark on student.student_id = mark.student_id join subject on mark.sub_id = subject.sub_id
+order by mark desc , student_name desc;
