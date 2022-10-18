7e9c2d5133036691a85278ec5c7f883dd6f40ca1
84384 <thanhhlongg2412@gmail.com>
2022-10-18 17:07:55 +0700

    update

--- Module3/case_study_database/case_study_task.sql
+++ Module3/case_study_database/case_study_task.sql
@@ -26,3 +26,15 @@ order by so_luong_dat_phong;
 	group by  hd.ma_hop_dong, kh.ma_khach_hang
 	order by ma_khach_hang ;
 -- TASK 6
+select dich_vu.ma_dich_vu,dich_vu.ten_dich_vu,dich_vu.dien_tich,dich_vu.chi_phi_thue,loai_dich_vu.ten_loai_dich_vu , hop_dong.ngay_lam_hop_dong
+from loai_dich_vu 
+join dich_vu on dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu
+left join hop_dong on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+where not (
+select dich_vu.ma_dich_vu
+from hop_dong 
+where not (year(hop_dong.ngay_lam_hop_dong) = 2021 and month(hop_dong.ngay_lam_hop_dong) in (1,2,3))
+)
+group by dich_vu.ma_dich_vu;
+-- TASK 7
+
--- Module3/ss2/practice3.sql
+++ Module3/ss2/practice3.sql
@@ -72,3 +72,12 @@ where student_name = 'Hung';
 select student.student_name , subject.sub_name, mark.mark 
 from student join mark on student.student_id = mark.student_id join subject on mark.sub_id = subject.sub_id
 order by mark desc , student_name desc;
+-- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
+select *, max(credit) from subject;
+-- Hiển thị các thông tin môn học có điểm thi lớn nhất.
+select subject.sub_id,subject.sub_name,subject.credit,subject.status,max(mark) from subject join mark on subject.sub_id = mark.sub_id;
+-- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
+select student.student_id,student_name,avg(mark)from student
+join mark on student.student_id = mark.student_id
+group by student.student_id
+order by avg(mark) desc;
