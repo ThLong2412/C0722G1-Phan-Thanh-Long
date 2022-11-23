ed0084dd4a84f895345b83b1c0c621d879e54703
84384 <thanhhlongg2412@gmail.com>
2022-11-23 16:48:15 +0700

    update

---
+++ Module4/ss3_views_and_thymeleaf/exersice/product-management/src/main/java/com/codegym/controller/ProductController.java
@@ -0,0 +1,71 @@
+package com.codegym.controller;
+
+import com.codegym.model.Product;
+import com.codegym.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+    @Autowired
+    private IProductService productService;
+
+    @GetMapping("")
+    public String index(Model model) {
+        List<Product> productList = productService.findAll();
+        model.addAttribute("productList", productList);
+        return "/index";
+    }
+
+    @GetMapping("/create")
+    public String showCreate(Model model) {
+        model.addAttribute("product", new Product());
+        return "/create";
+    }
+
+    @PostMapping("/save")
+    public String save(Product product, RedirectAttributes redirectAttributes) {
+        productService.save(product);
+        redirectAttributes.addFlashAttribute("messCreate", "Successfully added new");
+        return "redirect:/product";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String showUpdate(@PathVariable int id, Model model) {
+        Product product = productService.findById(id);
+        model.addAttribute("product", product);
+        return "/edit";
+    }
+
+    @PostMapping("/update")
+    public String update(Product product) {
+        productService.update(product.getId(), product);
+        return "redirect:/product";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String showDelete(@PathVariable int id, Model model) {
+        model.addAttribute("product", productService.findById(id));
+        return "/delete";
+    }
+
+    @PostMapping("/delete")
+    public String delete(Product product, RedirectAttributes redirectAttributes, Model model) {
+        productService.remove(product.getId());
+        redirectAttributes.addFlashAttribute("messDelete", "Removed customer successfully!");
+        model.addAttribute("product", product);
+        return "redirect:/product";
+    }
+
+    @GetMapping("/{id}/view")
+    public String view(Model model, @PathVariable int id) {
+        model.addAttribute("product", productService.findById(id));
+        return "/view";
+    }
+}
---
+++ Module4/ss3_views_and_thymeleaf/exersice/product-management/src/main/java/com/codegym/service/impl/ProductService.java
@@ -0,0 +1,47 @@
+package com.codegym.service.impl;
+
+import com.codegym.model.Product;
+import com.codegym.service.IProductService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ProductService implements IProductService {
+    private static final Map<Integer,Product> productList = new HashMap<>();
+    static {
+        productList.put(1,new Product(1,"Sách",45, "SGK", "VN"));
+        productList.put(2,new Product(2,"Vở",20, "4 ô ly", "VN"));
+        productList.put(3,new Product(3,"Bút",5, "mực xanh", "VN"));
+        productList.put(4,new Product(4,"Thước",10, "30cm", "VN"));
+    }
+    @Override
+    public List<Product> findAll() {
+        return new ArrayList<>(productList.values());
+    }
+
+    @Override
+    public void save(Product product) {
+        productList.put(product.getId(),product);
+    }
+
+    @Override
+    public Product findById(int id) {
+        return productList.get(id);
+    }
+
+    @Override
+    public void update(int id, Product product) {
+    productList.put(product.getId(), product);
+    }
+
+    @Override
+    public void remove(int id) {
+        productList.remove(id);
+    }
+
+
+}
